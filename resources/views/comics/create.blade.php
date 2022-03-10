632b9ccd11bb7df49650c082cafa88f0a4b0a05d
LorenzoBernardini88 <lorenzo.bernardini88@gmail.com>
2022-03-10 16:57:37 +0100

    mod form

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -6,36 +6,40 @@
     @csrf
     <div class="form-row">
         <div class="form-group col-md-6">
-            <label for="inputEmail4">Title</label>
-            <input type="email" class="form-control" id="inputEmail4" placeholder="Title">
+            <label for="title">Title</label>
+            <input type="text" class="form-control" name="title" id="title" placeholder="Title">
         </div>
         <div class="form-group col-md-3">
-            <label for="inputState">Type</label>
-            <select id="inputState" class="form-control">
+            <label for="series">Series</label>
+            <input type="text" class="form-control" name="series" id="series" placeholder="Series">
+        </div>
+        <div class="form-group col-md-3">
+            <label for="type">Type</label>
+            <select id="type" name="type" class="form-control">
                 <option>graphic novel</option>
                 <option>comic book</option>
             </select>
         </div> 
         </div>
         <div class="form-group">
-            <label for="" class="d-block">Description</label>
+            <label for="description" class="d-block">Description</label>
             <textarea name="description" id="description" cols="80" rows="" placeholder="Write Description"></textarea>
         </div>
         <div class="form-group">
-            <label for="inputAddress2">Thumb</label>
-            <input type="text" class="form-control" id="thumb" placeholder="URL Thumb">
+            <label for="thumb">Thumb</label>
+            <input type="text" class="form-control" name="thumb" id="thumb" placeholder="URL Thumb">
         </div>
         <div class="form-row">
             <div class="form-group col-md-2">
-                <label for="inputCity">Date-Sale</label>
-                <input type="text" class="form-control" id="date_sale">
+                <label for="date_sale">Date-Sale</label>
+                <input type="text" class="form-control" name="date_sale" id="date_sale">
             </div>
         <div class="form-group col-md-2">
-            <label for="inputZip">Price</label>
-            <input type="text" class="form-control" id="Price">
+            <label for="price">Price</label>
+            <input type="text" class="form-control" name="price" id="price">
         </div>
     </div>
 
-    <button type="submit" class="btn btn-primary">Sign in</button>
+    <button type="submit" class="btn btn-primary">Salva</button>
 </form>
 @endsection
