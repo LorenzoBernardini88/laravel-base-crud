93c00cd112e04be7eab8217717fde870ba5981a3
LorenzoBernardini88 <lorenzo.bernardini88@gmail.com>
2022-03-14 21:20:55 +0100

    added error message on edit

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -8,40 +8,49 @@
     <div class="form-row">
         <div class="form-group col-md-6">
             <label for="title">Title</label>
-            <input value="{{$comic->title}}" type="text" class="form-control" name="title" id="title" placeholder="Title">
+            <input value="{{old('title')?? $comic->title}}" type="text" class="form-control" name="title" id="title" placeholder="Title">
             
         </div>
         <div class="form-group col-md-3">
             <label for="series">Series</label>
-            <input  value="{{$comic->series}}" type="text" class="form-control" name="series" id="series" placeholder="Series">
+            <input  value="{{old('series')??$comic->series}}" type="text" class="form-control" name="series" id="series" placeholder="Series">
         </div>
         <div class="form-group col-md-3">
             <label for="type">Type</label>
             <select value="{{$comic->type}}" id="type" name="type" class="form-control">
-                <option>graphic novel</option>
-                <option>comic book</option>
+                <option value='graphic novel'{{old('type')=='graphic novel'? 'selected' : null}}>graphic novel</option>
+                <option value='comic book'{{old('type')=='comic' ? 'selected' : null}}>comic book</option>
             </select>
         </div> 
         </div>
         <div class="form-group">
             <label for="description" class="d-block">Description</label>
-            <textarea value="{{$comic->description}}" name="description" id="description" cols="80" rows="" placeholder="Write Description"></textarea>
+            <textarea value="{{old('description')?? $comic->description}}" name="description" id="description" cols="80" rows="" placeholder="Write Description"></textarea>
         </div>
         <div class="form-group">
             <label for="thumb">Thumb</label>
-            <input value="{{$comic->thumb}}" type="text" class="form-control" name="thumb" id="thumb" placeholder="URL Thumb">
+            <input value="{{old('thumb')?? $comic->thumb}}" type="text" class="form-control" name="thumb" id="thumb" placeholder="URL Thumb">
         </div>
         <div class="form-row">
             <div class="form-group col-md-2">
                 <label for="sale_date">Date-Sale</label>
-                <input value="{{$comic->sale_date}}" type="date" class="form-control" name="sale_date" id="sale_date">
+                <input value="{{old('sale_date')??$comic->sale_date}}" type="date" class="form-control" name="sale_date" id="sale_date">
             </div>
         <div class="form-group col-md-2">
             <label for="price">Price</label>
-            <input value="{{$comic->price}}" type="text" class="form-control" name="price" id="price">
+            <input value="{{old('price')??$comic->price}}" type="text" class="form-control" name="price" id="price">
         </div>
     </div>
 
     <button type="submit" class="btn btn-primary">Salva</button>
 </form>
+@if ($errors->any())
+<div class="alert alert-danger">
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
 @endsection
